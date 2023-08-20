7697063dafe674eb6eaadee2cac50f88395fa720
Leo Nguyen <lichnguyenz@gmail.com>
2023-08-20 00:06:47 -0500

    update

--- code/tile.py
+++ code/tile.py
@@ -10,6 +10,6 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
         # ../ mean to go up one directory. Currently in /code
-        self.image = pygame.image.load('/Users/trinhphuong/Documents/GitHub/ZeldaInPython/graphics/test/rock.png').convert_alpha()
+        self.image = pygame.image.load("../graphics/test/rock.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0,-10)
